2ef1869a9e80f7dccf38d0fec826bf0e7af8ce3f
x510u <x510u@DESKTOP-OSJ806G>
2022-05-13 09:23:33 +0200

    Soluzione prima parte lab04 tdp

--- src/main/java/it/polito/tdp/lab04/FXMLController.java
+++ src/main/java/it/polito/tdp/lab04/FXMLController.java
@@ -6,9 +6,17 @@ package it.polito.tdp.lab04;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.lab04.model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	Model model;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -16,8 +24,82 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="boxCorsi"
+    private ComboBox<String> boxCorsi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCercaIscritti"
+    private Button btnCercaIscritti; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtMatricola"
+    private TextField txtMatricola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCercaStudente"
+    private Button btnCercaStudente; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtNome"
+    private TextField txtNome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtCognome"
+    private TextField txtCognome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCercaCorsi"
+    private Button btnCercaCorsi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnIscrivi"
+    private Button btnIscrivi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtRisultato"
+    private TextArea txtRisultato; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnPulisci"
+    private Button btnPulisci; // Value injected by FXMLLoader
+
+    @FXML
+    void doCercaCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaIscritti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaStudente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doIscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doPulisci(ActionEvent event) {
+
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert boxCorsi != null : "fx:id=\"boxCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaIscritti != null : "fx:id=\"btnCercaIscritti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaStudente != null : "fx:id=\"btnCercaStudente\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnPulisci != null : "fx:id=\"btnPulisci\" was not injected: check your FXML file 'Scene.fxml'.";
 
+    }
+    public void setModel(Model model) {
+    	this.model=model;
+    	boxCorsi.getItems().addAll(model.getNomiCorsi());
     }
 }
--- src/main/java/it/polito/tdp/lab04/model/Model.java
+++ src/main/java/it/polito/tdp/lab04/model/Model.java
@@ -1,5 +1,35 @@
 package it.polito.tdp.lab04.model;
 
-public class Model {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import it.polito.tdp.lab04.DAO.CorsoDAO;
+import it.polito.tdp.lab04.DAO.StudenteDAO;
+
+public class Model {
+	
+	private CorsoDAO corsoDao;
+	private StudenteDAO studenteDao;
+	
+	
+
+	public Model() {
+		super();
+		this.corsoDao = new CorsoDAO();
+		this.studenteDao = new StudenteDAO();
+	}
+	
+
+
+
+	public List<String> getNomiCorsi(){
+		List<String> result= new ArrayList<>();
+		for(Corso c : this.corsoDao.getTuttiICorsi()) {
+			result.add(c.getNome());
+		}
+		result.add("");
+		Collections.sort(result);
+		return result;
+	}
 }
